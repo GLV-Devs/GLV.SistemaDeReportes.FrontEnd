7149ac73ac14a9fab241cb15cc47739cc0b9953a
Jesus David Lozano <jesusdavidlozanomarin@gmail.com>
2024-03-23 11:04:02 -0400

    listado de personal y cambios al NavBar
    Se agrego el boton de cerra sesion pero aun no tiene funcionalidad, se establecio la forma de mostrar los trabajadores en la lista de empleados con botones para ver y editar informacion, aun sin funcionalidad.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,7 +11,10 @@ const NavBar = () => {
             <Button variant="contained" onClick={() => navigate('/main/personas')}>Personas</Button>
             <Button variant="contained" onClick={() => navigate('/main/proyectos')}>Proyectos</Button>
             <Button variant="contained" onClick={() => navigate('/main/productos')}>Productos</Button>
-            <h3>Bienvenido, Jesus Lozano</h3>
+            <div className="sesion">
+                <h3>Bienvenido, Jesus Lozano</h3>
+                <Button variant="contained" color='error'>Salir</Button>
+            </div>
         </div>
     )
 }
--- src/routes/ListPersonal.jsx
+++ src/routes/ListPersonal.jsx
@@ -2,17 +2,35 @@ import { TextField, Button } from "@mui/material";
 
 const ListPersonal = () => {
 
-    const infoPruba = [{
+    const infoPrueba = [{
         name: 'Jesus Lozano',
         address: 'Av Uni',
-        idType: "",
-        idNumber: e.target[6].value,
-        phone: e.target[8].value,
+        idType: "V",
+        idNumber: '28.288.269',
+        phone: '0412-7859759',
+    },{
+        name: 'Diego Garcia',
+        address: 'Bella Vista',
+        idType: "E",
+        idNumber: '27...',
+        phone: '0414',
     }]
 
     return(
         <div className="AddPersonal">
             <h1>Lista del personal</h1>
+            {infoPrueba.map( (worker) => (
+                <div className="LI">
+                    <div className="info">
+                        <h4>{worker.name}</h4>
+                        <h4>{worker.idType} {worker.idNumber}</h4>
+                    </div>
+                    <div className="Buttons">
+                        <Button variant='contained'>Editar</Button>
+                        <Button variant="contained">Ver info</Button>
+                    </div>
+                </div>
+            ) )}
         </div>
     )
 }
