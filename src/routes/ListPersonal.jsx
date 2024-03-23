4042aa589a78f94fdbf294b1c4437e4042a9e985
Jesus David Lozano <jesusdavidlozanomarin@gmail.com>
2024-03-22 22:33:13 -0400

    Apartado de Personal comenzado
    Se agregaron las opciones disponibles en el sub-menu de personas, se estan agregando datos de prueba y ajustando estilos. aun no se integra funcionalidad.

--- src/routes/ListPersonal.jsx
+++ src/routes/ListPersonal.jsx
@@ -1,6 +1,15 @@
 import { TextField, Button } from "@mui/material";
 
 const ListPersonal = () => {
+
+    const infoPruba = [{
+        name: 'Jesus Lozano',
+        address: 'Av Uni',
+        idType: "",
+        idNumber: e.target[6].value,
+        phone: e.target[8].value,
+    }]
+
     return(
         <div className="AddPersonal">
             <h1>Lista del personal</h1>
